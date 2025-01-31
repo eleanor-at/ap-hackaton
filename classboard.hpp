b12165211ae5ccf0b28db7f106432d1cdb1afefc
Aude-V <aude.vendeuvre@etu.minesparis.psl.eu>
2025-01-31 11:55:45 +0100

    reset

--- classboard.hpp
+++ classboard.hpp
@@ -6,6 +6,15 @@
 #include <string>
 #include <cstdlib>
 
+bool in(int x,std::vector<int> vect)
+{
+    for (auto e:vect)
+    {
+        if (e==x){return true;}
+    }
+    return false;
+}
+
 class Board
 {
 private:
@@ -13,7 +22,18 @@ private:
 
 public:
     Board() : tab(9) {};
-    void reset();
+    void reset() //crée un board avec les nombres rangés aléatoirement et le -1 en bas à droite
+    {
+        std::vector<int> rempli;
+        tab[8]=-1;
+        for (int i=0;i<8;i++)
+        {
+            int x=rand()%9;
+            while (in(x,rempli)) {x=rand()%9;}
+            rempli.push_back(x);
+            tab[i]=x;
+        }
+    }
     void swap(int x, int y) // échange deux éléments du board, prend en arguments les positions de ces éléments
     {
         int t = tab[x];
